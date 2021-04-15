5d862d5fd5d54b4afc23c49e538f6a2f75858d2d
t-rutten <t.rutten15@gmail.com>
2021-04-15 19:37:43 -0400

    add docs

--- lib/mnist.ex
+++ lib/mnist.ex
@@ -26,6 +26,24 @@ defmodule AxonDatasets.MNIST do
     transform.({labels, {:u, 8}, {n_labels}})
   end
 
+  @doc """
+  Downloads the MNIST dataset or fetches it locally.
+
+  ## Options
+  * `datapath` - path where the dataset .gz should be stored locally
+  * `transform_images/1` - accepts accept a tuple like
+      `{binary_data, tensor_type, data_shape}` which can be used for
+      converting the `binary_data` to a tensor with a function like
+          fn {labels_binary, type, _shape} ->
+            labels_binary
+            |> Nx.from_binary(type)
+            |> Nx.new_axis(-1)
+            |> Nx.equal(Nx.tensor(Enum.to_list(0..9)))
+            |> Nx.to_batched_list(32)
+          end
+  * `transform_labels/1` - similar to `transform_images/1` but applied to
+      dataset labels
+  """
   def download(opts \\ []),
     do: {download_images(opts), download_labels(opts)}
 end
