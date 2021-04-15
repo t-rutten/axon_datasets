ca28a812dc62e1da209576a70695cc1c174cbb2d
t-rutten <t.rutten15@gmail.com>
2021-04-15 18:43:51 -0400

    init repo

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule AxonDatasets.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :axon_datasets,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:nx, "~> 0.1.0-dev", github: "elixir-nx/nx", branch: "main", sparse: "nx", override: true}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
---
+++ test/axon_datasets_test.exs
@@ -0,0 +1,8 @@
+defmodule AxonDatasetsTest do
+  use ExUnit.Case
+  doctest AxonDatasets
+
+  test "greets the world" do
+    assert AxonDatasets.hello() == :world
+  end
+end
